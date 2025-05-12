92bdbf7bd4067774576580439f6b56717b272891
Shivam Sharma <120017051+shivam2419@users.noreply.github.com>
2025-05-12 12:02:32 +0530

    Update settings.py

--- EWaste/settings.py
+++ EWaste/settings.py
@@ -142,6 +142,12 @@ USE_I18N = True
 
 USE_TZ = True
 
+# CSRF allowed origins
+CSRF_TRUSTED_ORIGINS = [
+    "https://scrapbridge-api.onrender.com/",  # Include www if used
+]
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
